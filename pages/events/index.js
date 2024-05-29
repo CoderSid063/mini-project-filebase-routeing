bcb84f8dce2dfd89c6f1929b7ee072da1e84731b
CoderSid063 <sudhansupanda063@gmail.com>
2024-05-29 12:35:30 +0530

    feat : add filter form for filltering events and navigate to them

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,22 @@
+import { Fragment } from "react";
 import EventList from "../../components/events/event-list";
 import { getAllEvents } from "../../dummy-data";
+import EventsSearch from "../../components/events/events-search";
+import { useRouter } from "next/router";
 
 export default function AllEvenetPage(params) {
   const events = getAllEvents();
+  const router = useRouter();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
+
   return (
-    <div>
+    <Fragment>
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
-    </div>
+    </Fragment>
   );
 }
